975990944c2e59833cfca2bb88029ae4d580c128
Aldungarov Artem <shepardcmndr@gmail.com>
2022-12-21 22:03:13 +0400

    homework 10.9 done

--- src/main/java/practice/EmailList.java
+++ src/main/java/practice/EmailList.java
@@ -1,19 +1,29 @@
 package practice;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class EmailList {
 
+    private final TreeSet<String> emailList = new TreeSet<>();
+
     public void add(String email) {
-        // TODO: валидный формат email добавляется, email это строка, она быть может любой
-        // принять решение добавлять аргумент email или нет должен этот метод
+        if (email.isEmpty()) {
+            System.out.println("Неверный формат email");
+            return;
+        }
+        email = email.toLowerCase();
+        String pattern = "^(\\w+)@(\\S+){2,6}\\.([A-Za-z]{2,})";
+        boolean matches = Pattern.compile(pattern).matcher(email).matches();
+        if (matches) {
+            emailList.add(email);
+        } else {
+            System.out.println("Неверный формат email");
+        }
     }
 
     public List<String> getSortedEmails() {
-        // TODO: возвращается сортированный список электронных адресов в алфавитном порядке
-        return new ArrayList<>();
+        return new ArrayList<>(emailList);
     }
 
 }
